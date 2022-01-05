7eb431083c49ba4a6d2241d72db4a274d83c7bce
Carlos Motiño <carlos.motino@elcatex.com>
2022-01-05 07:50:00 -0600

    Primer commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png">  
+    <h1>Hola Mundo</h1>  
+    <button @click="goToDayBook" class="btn btn-primary">DayBook</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-success">Success</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  methods:{
+    goToDayBook() {
+      this.$router.push({name:'no-entry'})
+    }
   }
 }
 </script>
